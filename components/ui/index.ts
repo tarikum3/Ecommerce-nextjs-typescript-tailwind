afd62e8857594d98e7caf683ef08b324ddd1d786
tariku <tarikum3@gmail.com>
2023-12-14 03:58:42 -0800

    modalinda ui

--- components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-
-export { default as Logo } from './Logo'
-export { default as Button } from './Button'
-
-export { default as Modal } from './Modal'
-
-export { default as Input } from './Input'
-export { default as ErrorMessage } from './ErrorMessage'
-export { useUI } from './context'
