a9558f8cebd01dc325ba19a365950fa1fd7850b2
tariku <tarikum3@gmail.com>
2025-12-15 18:51:09 +0300

    productdetail

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,31 +1,31 @@
 
-// // import { Hero } from "@/app/components/sections/Hero"
-// import  Hero  from "@/app/components/sections/hero/Hero"
-// import CategorySection from "@/app/components/Category/CategorySection";
-// import TestimonialsSection from "@/app/components/sections/testimonial/TestimonialsSection";
-// import FeaturedSection from "@/app/components/sections/featured/FeaturedSection";
-// import FavoritesSection from "@/app/components/sections/favorite/FavoritesSection";
-// //export const dynamic = "force-dynamic";
+// import { Hero } from "@/app/components/sections/Hero"
+import  Hero  from "@/app/components/sections/hero/Hero"
+import CategorySection from "@/app/components/Category/CategorySection";
+import TestimonialsSection from "@/app/components/sections/testimonial/TestimonialsSection";
+import FeaturedSection from "@/app/components/sections/featured/FeaturedSection";
+import FavoritesSection from "@/app/components/sections/favorite/FavoritesSection";
+//export const dynamic = "force-dynamic";
 
-// //export const revalidate = 3600;
-// //export const revalidate = 3600;
-// export const metadata = {
-//   description: "Modalinda shop.",
-//   openGraph: {
-//     type: "website",
-//   },
-// };
-// export default async function Home() {
-//   return (
-//     <>
-//    <Hero />
-//    <CategorySection />
-//    <TestimonialsSection />
-//    <FeaturedSection />
-//    <FavoritesSection />
-//     </>
-//   );
-// }
+//export const revalidate = 3600;
+//export const revalidate = 3600;
+export const metadata = {
+  description: "Modalinda shop.",
+  openGraph: {
+    type: "website",
+  },
+};
+export default async function Home() {
+  return (
+    <>
+   <Hero />
+   <CategorySection />
+   <TestimonialsSection />
+   <FeaturedSection />
+   <FavoritesSection />
+    </>
+  );
+}
 
 
 
